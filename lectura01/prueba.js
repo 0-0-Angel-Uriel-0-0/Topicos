75bbfe44bc1b1cade90526167f3aa706c65e6529
Angel Uriel Ceseña Alvarez <al21760123@ite.edu.mx>
2024-02-26 12:53:58 -0800

    Agrege contenedores para los parrafos y quite carpetas inecesarias

--- lectura01/prueba.js
+++
@@ -1,3 +0,0 @@
-const arreglo = ["oscar", "angel", "jazmin", "winnie", "dulce"];
-
-console.log(arreglo.sort());
--- script3/parrafosInteligentes.js
+++ script3/parrafosInteligentes.js
@@ -43,8 +43,27 @@ const parrafoAleatorio2 = generarParrafoAleatorio();
 const parrafoAleatorio3 = generarParrafoAleatorio();
 const parrafoAleatorio4 = generarParrafoAleatorio();
 
-console.log(parrafoAleatorio0);
-console.log(parrafoAleatorio1);
-console.log(parrafoAleatorio2);
-console.log(parrafoAleatorio3);
-console.log(parrafoAleatorio4);
+let parrafo0 = document.createElement("div");
+parrafo0.style.padding = "10px";
+parrafo0.innerHTML = parrafoAleatorio0;
+document.body.appendChild(parrafo0);
+
+let parrafo1 = document.createElement("div");
+parrafo1.style.padding = "10px";
+parrafo1.innerHTML = parrafoAleatorio1;
+document.body.appendChild(parrafo1);
+
+let parrafo2 = document.createElement("div");
+parrafo2.style.padding = "10px";
+parrafo2.innerHTML = parrafoAleatorio2;
+document.body.appendChild(parrafo2);
+
+let parrafo3 = document.createElement("div");
+parrafo3.style.padding = "10px";
+parrafo3.innerHTML = parrafoAleatorio3;
+document.body.appendChild(parrafo3);
+
+let parrafo4 = document.createElement("div");
+parrafo4.style.padding = "10px";
+parrafo4.innerHTML = parrafoAleatorio4;
+document.body.appendChild(parrafo4);
